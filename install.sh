351dd2831f9b50e845229865585eb4d7589520a0
Kosma <56829298+kosma-hyzoe@users.noreply.github.com>
2023-09-06 13:23:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 NVIM_DOWNLOAD_URL="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 install_libfuse() {
   if ! dpkg -s libfuse2 &>/dev/null; then
     echo "Installing libfuse2..."
     sudo add-apt-repository -y universe &>/dev/null
-    install libfuse2 > /dev/null
+    sudo apt install libfuse2 > /dev/null
   fi
 }
 
