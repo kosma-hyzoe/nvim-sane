6cc358d69ec970a4d5c14d3477076c6bae3a7c0e
Marcin Hyzorek <m.hyzorek@samsung.com>
2023-11-24 12:00:01 +0100

    automated quick commit

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/kosma/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?.lua;/home/kosma/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?/init.lua;/home/kosma/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?.lua;/home/kosma/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/kosma/.cache/nvim/packer_hererocks/2.1.1692716794/lib/lua/5.1/?.so"
+local package_path_str = "/home/m.hyzorek/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/m.hyzorek/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/m.hyzorek/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/m.hyzorek/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/m.hyzorek/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,12 +76,12 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["lsp-zero.nvim"] = {
@@ -89,27 +89,27 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/opt/lsp-zero.nvim",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/opt/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
@@ -117,37 +117,37 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = false,
     only_cond = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/opt/nvim-treesitter",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/opt/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-treesitter-context"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
     url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
   },
   ["onedark.nvim"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/onedark.nvim",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/onedark.nvim",
     url = "https://github.com/navarasu/onedark.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   playground = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/playground",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/playground",
     url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["suda.vim"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/suda.vim",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/suda.vim",
     url = "https://github.com/lambdalisue/suda.vim"
   },
   ["telescope.nvim"] = {
@@ -155,44 +155,44 @@ _G.packer_plugins = {
     loaded = false,
     needs_bufread = true,
     only_cond = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/opt/telescope.nvim",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/opt/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-commentary"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/vim-commentary",
     url = "https://github.com/tpope/vim-commentary"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-highlightedyank"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/vim-highlightedyank",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/vim-highlightedyank",
     url = "https://github.com/machakann/vim-highlightedyank"
   },
   ["vim-multiple-cursors"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/vim-multiple-cursors",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/vim-multiple-cursors",
     url = "https://github.com/terryma/vim-multiple-cursors"
   },
   ["vim-surround"] = {
     loaded = true,
-    path = "/home/kosma/.local/share/nvim/site/pack/packer/start/vim-surround",
+    path = "/home/m.hyzorek/.local/share/nvim/site/pack/packer/start/vim-surround",
     url = "https://github.com/tpope/vim-surround"
   }
 }
 
 time([[Defining packer_plugins]], false)
 -- Conditional loads
-time([[Conditional loading of telescope.nvim]], true)
-  require("packer.load")({"telescope.nvim"}, {}, _G.packer_plugins)
-time([[Conditional loading of telescope.nvim]], false)
 time([[Conditional loading of lsp-zero.nvim]], true)
   require("packer.load")({"lsp-zero.nvim"}, {}, _G.packer_plugins)
 time([[Conditional loading of lsp-zero.nvim]], false)
+time([[Conditional loading of telescope.nvim]], true)
+  require("packer.load")({"telescope.nvim"}, {}, _G.packer_plugins)
+time([[Conditional loading of telescope.nvim]], false)
 time([[Conditional loading of nvim-treesitter]], true)
   require("packer.load")({"nvim-treesitter"}, {}, _G.packer_plugins)
 time([[Conditional loading of nvim-treesitter]], false)
