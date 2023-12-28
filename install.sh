26463d943c6c23c968a03de30dfa20290a4e55a5
Marcin Hyzorek <m.hyzorek@samsung.com>
2023-12-28 15:51:12 +0100

    added apt prerequisites to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+sudo apt-get install -y ninja-build gettext cmake unzip curl
+
 git clone https://github.com/neovim/neovim
 cd neovim
 git checkout stable
@@ -8,4 +10,3 @@ sudo make install && cd .. && rm -rf neovim
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
