12d4c06ff8dc5a7326a87f5e5d3bc7fcaf2ba61e
Marcin Hyzorek <m.hyzorek@samsung.com>
2024-01-18 17:55:21 +0100

    automated quick commit

--- bin-install.sh
+++ bin-install.sh
@@ -1,5 +1,11 @@
 wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
+tar xzvf nvim-*.tar.gz
+rm -f nvim-*.tar.gz
 
 mkdir -p ~/.local/bin
-cp nvim-linux64/bin/nvim ~/.local/bin && rm -rf nvim-linux64
+cp -rf nvim-linux64 ~/.local && rm -rf nvim-linux64
+mkdir -p ~/.config
+echo 'export "PATH=$PATH:$HOME/.local/nvim-linux64/bin"' >> ~/.config/localshrc
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
