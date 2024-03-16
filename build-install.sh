2167b37e7b14f59351c07222e8055c20038ef9bf
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-03-16 11:28:47 +0100

    export path for build install

--- build-install.sh
+++ build-install.sh
@@ -8,3 +8,7 @@ sudo make install && cd .. && rm -rf neovim
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+if grep -q "./local/nvim-linux64/bin" ~/.config/aliasrc &>/dev/null; then
+    echo "export PATH='\$PATH:\$HOME/.local/nvim-linux64/bin'" >> ~/.config/aliasrc
+fi
