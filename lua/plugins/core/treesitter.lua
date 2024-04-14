c179e43096301bf66333eb8f03d5a88fc249d802
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2024-04-14 21:51:58 +0200

    yup, multifile plugin config

---
+++ lua/plugins/core/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { "ruby", "markdown" },
+		},
+		indent = { enable = true, disable = { "ruby", "markdown" } },
+	},
+	config = function(_, opts)
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup(opts)
+	end,
+}
