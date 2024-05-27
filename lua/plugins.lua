7507ad2a441ff82231dd7fa0fcf272c91cc8545f
Marcin Hyzorek <m.hyzorek@samsung.com>
2024-05-27 18:47:55 +0200

    automated quick commit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,10 +5,18 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	"lambdalisue/suda.vim",
-	"tpope/vim-sleuth",
+if vim.vscode then
+	require("lazy").setup({
+		"tpope/vim-sleuth",
 
-	{ import = "plugins.core" },
-	{ import = "plugins.auxilary" },
-}, {})
+		{ import = "plugins.vscode" },
+	}, {})
+else
+	require("lazy").setup({
+		"lambdalisue/suda.vim",
+		"tpope/vim-sleuth",
+
+		{ import = "plugins.core" },
+		{ import = "plugins.auxilary" },
+	}, {})
+end
