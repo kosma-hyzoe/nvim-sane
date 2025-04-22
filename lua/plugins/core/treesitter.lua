e79864cd7247cd882f9ac174083ab51d64210938
Kosma Marcin Hyżorek <kosma.hyzoe@gmail.com>
2025-04-22 11:07:00 +0200

    treesitter-context is back bby!

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -13,5 +13,14 @@ return {
 	config = function(_, opts)
 		---@diagnostic disable-next-line: missing-fields
 		require("nvim-treesitter.configs").setup(opts)
+		require("treesitter-context").setup({
+			enable = true, -- Enable this plugin
+			max_lines = 0, -- How many lines the window can be
+			min_window_height = 0, -- Minimum height of the context window
+			line_numbers = true, -- Show line numbers
+			multiline_threshold = 20, -- The threshold for multiline contexts
+			trim_scope = "outer", -- Trim scope options: 'outer', 'inner'
+			mode = "topline", -- Options: 'topline', 'cursor' or 'smart'
+		})
 	end,
 }
